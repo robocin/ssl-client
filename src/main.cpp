23f3e8cd6187151e54d3a0c5677d2d59abab5916
José Cruz <jvsc@cin.ufpe.br>
2020-12-13 02:41:11 -0300

    refactor(main.cpp): changing from printf to qDebug

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,42 @@
 // author  Renato Sousa, 2018
-#include <cstdio>
+#include <QDebug>
 #include <QtNetwork>
 #include <ssl-client/ssl-client.h>
 
 void printRobotInfo(const SSL_DetectionRobot& robot) {
-  printf("CONF=%4.2f ", robot.confidence());
+  qDebug("CONF=%4.2f ", robot.confidence());
   if (robot.has_robot_id()) {
-    printf("ID=%3d ", robot.robot_id());
+    qDebug("ID=%3d ", robot.robot_id());
   } else {
-    printf("ID=N/A ");
+    qDebug("ID=N/A ");
   }
-  printf(" HEIGHT=%6.2f POS=<%9.2f,%9.2f> ",
+  qDebug(" HEIGHT=%6.2f POS=<%9.2f,%9.2f> ",
          robot.height(),
          robot.x(),
          robot.y());
   if (robot.has_orientation()) {
-    printf("ANGLE=%6.3f ", robot.orientation());
+    qDebug("ANGLE=%6.3f ", robot.orientation());
   } else {
-    printf("ANGLE=N/A    ");
+    qDebug("ANGLE=N/A    ");
   }
-  printf("RAW=<%8.2f,%8.2f>\n", robot.pixel_x(), robot.pixel_y());
+  qDebug("RAW=<%8.2f,%8.2f>\n", robot.pixel_x(), robot.pixel_y());
 }
 
 int main(int argc, char* argv[]) {
   (void) argc;
   (void) argv;
-  RoboCupSSLClient client;
+
+  RoboCupSSLClient client(10020, "224.5.23.2");
+
   client.open(true);
   SSL_WrapperPacket packet;
 
-  GrSim_Client grSim_client;
+  GrSim_Client_Example grSim_client;
+  grSim_client.setPortAndAddress(10020, "224.5.23.2");
 
   while (true) {
     if (client.receive(packet)) {
-      printf(
+      qDebug(
           "-----Received Wrapper Packet---------------------------------------------\n");
       // see if the packet contains a robot detection frame:
       if (packet.has_detection()) {
@@ -41,18 +44,18 @@ int main(int argc, char* argv[]) {
         // Display the contents of the robot detection results:
         double t_now = GetTimeSec();
 
-        printf("-[Detection Data]-------\n");
+        qDebug("-[Detection Data]-------\n");
         // Frame info:
-        printf("Camera ID=%d FRAME=%d T_CAPTURE=%.4f\n",
+        qDebug("Camera ID=%d FRAME=%d T_CAPTURE=%.4f\n",
                detection.camera_id(),
                detection.frame_number(),
                detection.t_capture());
 
-        printf("SSL-Vision Processing Latency                   %7.3fms\n",
+        qDebug("SSL-Vision Processing Latency                   %7.3fms\n",
                (detection.t_sent() - detection.t_capture()) * 1000.0);
-        printf("Network Latency (assuming synched system clock) %7.3fms\n",
+        qDebug("Network Latency (assuming synched system clock) %7.3fms\n",
                (t_now - detection.t_sent()) * 1000.0);
-        printf("Total Latency   (assuming synched system clock) %7.3fms\n",
+        qDebug("Total Latency   (assuming synched system clock) %7.3fms\n",
                (t_now - detection.t_capture()) * 1000.0);
         int balls_n = detection.balls_size();
         int robots_blue_n = detection.robots_blue_size();
@@ -61,24 +64,24 @@ int main(int argc, char* argv[]) {
         // Ball info:
         for (int i = 0; i < balls_n; i++) {
           SSL_DetectionBall ball = detection.balls(i);
-          printf("-Ball (%2d/%2d): CONF=%4.2f POS=<%9.2f,%9.2f> ",
+          qDebug("-Ball (%2d/%2d): CONF=%4.2f POS=<%9.2f,%9.2f> ",
                  i + 1,
                  balls_n,
                  ball.confidence(),
                  ball.x(),
                  ball.y());
           if (ball.has_z()) {
-            printf("Z=%7.2f ", ball.z());
+            qDebug("Z=%7.2f ", ball.z());
           } else {
-            printf("Z=N/A   ");
+            qDebug("Z=N/A   ");
           }
-          printf("RAW=<%8.2f,%8.2f>\n", ball.pixel_x(), ball.pixel_y());
+          qDebug("RAW=<%8.2f,%8.2f>\n", ball.pixel_x(), ball.pixel_y());
         }
 
         // Blue robot info:
         for (int i = 0; i < robots_blue_n; i++) {
           SSL_DetectionRobot robot = detection.robots_blue(i);
-          printf("-Robot(B) (%2d/%2d): ", i + 1, robots_blue_n);
+          qDebug("-Robot(B) (%2d/%2d): ", i + 1, robots_blue_n);
           printRobotInfo(robot);
           if (robot.x() <= 0) {
             grSim_client.sendCommand(1.0, i);
@@ -90,7 +93,7 @@ int main(int argc, char* argv[]) {
         // Yellow robot info:
         for (int i = 0; i < robots_yellow_n; i++) {
           SSL_DetectionRobot robot = detection.robots_yellow(i);
-          printf("-Robot(Y) (%2d/%2d): ", i + 1, robots_yellow_n);
+          qDebug("-Robot(Y) (%2d/%2d): ", i + 1, robots_yellow_n);
           printRobotInfo(robot);
         }
       }
@@ -98,42 +101,42 @@ int main(int argc, char* argv[]) {
       // see if packet contains geometry data:
       if (packet.has_geometry()) {
         const SSL_GeometryData& geom = packet.geometry();
-        printf("-[Geometry Data]-------\n");
+        qDebug("-[Geometry Data]-------\n");
 
         const SSL_GeometryFieldSize& field = geom.field();
-        printf("Field Dimensions:\n");
-        printf("  -field_length=%d (mm)\n", field.field_length());
-        printf("  -field_width=%d (mm)\n", field.field_width());
-        printf("  -boundary_width=%d (mm)\n", field.boundary_width());
-        printf("  -goal_width=%d (mm)\n", field.goal_width());
-        printf("  -goal_depth=%d (mm)\n", field.goal_depth());
-        printf("  -field_lines_size=%d\n", field.field_lines_size());
-        printf("  -field_arcs_size=%d\n", field.field_arcs_size());
+        qDebug("Field Dimensions:\n");
+        qDebug("  -field_length=%d (mm)\n", field.field_length());
+        qDebug("  -field_width=%d (mm)\n", field.field_width());
+        qDebug("  -boundary_width=%d (mm)\n", field.boundary_width());
+        qDebug("  -goal_width=%d (mm)\n", field.goal_width());
+        qDebug("  -goal_depth=%d (mm)\n", field.goal_depth());
+        qDebug("  -field_lines_size=%d\n", field.field_lines_size());
+        qDebug("  -field_arcs_size=%d\n", field.field_arcs_size());
 
         int calib_n = geom.calib_size();
         for (int i = 0; i < calib_n; i++) {
           const SSL_GeometryCameraCalibration& calib = geom.calib(i);
-          printf("Camera Geometry for Camera ID %d:\n", calib.camera_id());
-          printf("  -focal_length=%.2f\n", calib.focal_length());
-          printf("  -principal_point_x=%.2f\n", calib.principal_point_x());
-          printf("  -principal_point_y=%.2f\n", calib.principal_point_y());
-          printf("  -distortion=%.2f\n", calib.distortion());
-          printf("  -q0=%.2f\n", calib.q0());
-          printf("  -q1=%.2f\n", calib.q1());
-          printf("  -q2=%.2f\n", calib.q2());
-          printf("  -q3=%.2f\n", calib.q3());
-          printf("  -tx=%.2f\n", calib.tx());
-          printf("  -ty=%.2f\n", calib.ty());
-          printf("  -tz=%.2f\n", calib.tz());
+          qDebug("Camera Geometry for Camera ID %d:\n", calib.camera_id());
+          qDebug("  -focal_length=%.2f\n", calib.focal_length());
+          qDebug("  -principal_point_x=%.2f\n", calib.principal_point_x());
+          qDebug("  -principal_point_y=%.2f\n", calib.principal_point_y());
+          qDebug("  -distortion=%.2f\n", calib.distortion());
+          qDebug("  -q0=%.2f\n", calib.q0());
+          qDebug("  -q1=%.2f\n", calib.q1());
+          qDebug("  -q2=%.2f\n", calib.q2());
+          qDebug("  -q3=%.2f\n", calib.q3());
+          qDebug("  -tx=%.2f\n", calib.tx());
+          qDebug("  -ty=%.2f\n", calib.ty());
+          qDebug("  -tz=%.2f\n", calib.tz());
 
           if (calib.has_derived_camera_world_tx() &&
               calib.has_derived_camera_world_ty() &&
               calib.has_derived_camera_world_tz()) {
-            printf("  -derived_camera_world_tx=%.f\n",
+            qDebug("  -derived_camera_world_tx=%.f\n",
                    calib.derived_camera_world_tx());
-            printf("  -derived_camera_world_ty=%.f\n",
+            qDebug("  -derived_camera_world_ty=%.f\n",
                    calib.derived_camera_world_ty());
-            printf("  -derived_camera_world_tz=%.f\n",
+            qDebug("  -derived_camera_world_tz=%.f\n",
                    calib.derived_camera_world_tz());
           }
         }
